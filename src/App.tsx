0faaa27d940fa4df6e60485f60e6f786c11ee094
TecJP <tec.joao.freitas@gmail.com>
2021-12-14 10:38:14 -0300

    GlobalStyle and route and private route

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import { GlobalStyle } from "./styles/global";
+
+import { SignIn } from "./pages/SignIn";
+import { Dashboard } from "./pages/Dashboard";
+
+import { PrivateRoute } from './components/PrivateRoute';
+
+import { AuthContextProvider } from "./contexts/AuthContext";
+
 export function App() {
+  const [authenticated, setAuthenticated] = useState(false);
+
   return (
-    <h1>Hello World!!</h1>
+    <>
+      <BrowserRouter>
+        <AuthContextProvider>
+          <Switch>
+            <Route path='/' exact component={() => <SignIn setAuthenticated={setAuthenticated} />} />
+            <PrivateRoute authenticationPath="/" isAuthenticated={authenticated} path="/dashboard" component={Dashboard} />
+          </Switch>
+        </AuthContextProvider>
+      </BrowserRouter>
+      <GlobalStyle />
+    </>
   );
 }
+
